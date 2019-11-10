cbc84c180ab940c7f0867020da9cccf1641970c6
wfjones6 <55958934+wfjones6@users.noreply.github.com>
2019-11-09 19:55:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ function generateHTML(data) {
   
 <!DOCTYPE html>
 <html lang="en">
-
    <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
